72c25f591f423045e2b86167936208eb688337b2
vsharma <vaishalisharmaukl@gmail.com>
2025-11-09 20:07:15 +0530

    creating function that increment by 1

---
+++ Javascript/challenge 5/index.js
@@ -0,0 +1,16 @@
+// create a function that increment the lapsCompleted variable with one 
+// Run this three times
+let lapsCompleted = 0;
+
+function incrementByOne(){
+    lapsCompleted = lapsCompleted + 1;
+}
+
+incrementByOne();    // 1
+incrementByOne();    // 2
+incrementByOne();    // 3
+
+
+
+console.log(lapsCompleted);
+// Answer should be 3
