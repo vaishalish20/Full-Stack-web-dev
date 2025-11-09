15bf27be08d8b79278b5cb64ea32face299d3699
vsharma <vaishalisharmaukl@gmail.com>
2025-11-09 18:59:29 +0530

    completing dogAge and bonusPoint challenge

--- Javascript/challenge 1/index.js
+++ Javascript/challenge 1/index.js
@@ -1,5 +1,29 @@
 // document.getElementById("count-el").innerHTML = 2 ;
 
 // challenge 1
-let myAge=25;
-console.log(myAge);
+// let myAge=25;
+// console.log(myAge);
+
+// we can not console.log first without initilializing.
+
+// let firstnum = 12;
+// let secondnum = 18;
+// let count = firstnum + secondnum
+// console.log(count);
+
+// challenge 2
+// let myAge=25;
+// let humanDogRatio =7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+// challenge 3
+let bonusPoint = 50;
+bonusPoint = bonusPoint + 100;
+console.log(bonusPoint);
+bonusPoint = bonusPoint - 25;
+console.log(bonusPoint);
+bonusPoint = bonusPoint + 70;
+console.log(bonusPoint);
+
+// 50 + 100 = 150 - 25 = 125 + 70 = 195
