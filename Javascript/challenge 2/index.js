2b09d9e537f9321ef53de7671584fca4cb9d9196
vsharma <vaishalisharmaukl@gmail.com>
2025-11-09 19:39:22 +0530

    learning about functions

---
+++ Javascript/challenge 2/index.js
@@ -0,0 +1,27 @@
+// Setting up the race 
+
+// console.log(5);
+// console.log(4);
+// console.log(3);
+// console.log(2);
+// console.log(1);
+
+// Note: Instead of count it again and again, we are creating functions
+
+// Following is the function declaration 
+function countDown() {
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
+}
+
+// following is function calling
+countDown()
+// GO
+// Players are running the race.
+// Race is finished
+
+// Get ready for the new race.
+countDown()
