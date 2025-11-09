9e9099538fc56d27af0cba1e9afd0aad91fc6706
vsharma <vaishalisharmaukl@gmail.com>
2025-11-09 19:54:03 +0530

    function that log out the sum

---
+++ Javascript/challenge 4/index.js
@@ -0,0 +1,13 @@
+// creating a function that logs out the sun of all the lap times.
+
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+function sumOfLaps(){
+    let sum = lap1 + lap2 + lap3;
+    console.log(sum);
+    
+}
+
+sumOfLaps()
