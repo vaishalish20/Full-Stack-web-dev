551db638da66404706b7a202f9d8f0d09eba5fc8
vsharma <vaishalisharmaukl@gmail.com>
2025-11-10 21:32:25 +0530

    learning how to concatinate strings

--- Javascript/challenge 6/index.js
+++ Javascript/challenge 6/index.js
@@ -3,4 +3,6 @@ let username = "nod"
 // to create a string that show message
 let message = "You have three new notifications"
 
-console.log(message + ", " + username);
+// create a varibale messageTouser containing the message to user.
+let messageToUser = message + ", " + username;
+console.log(messageToUser);
