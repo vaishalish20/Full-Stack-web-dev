0acad40631e7f24646a7f95d97971e84c29d2764
vsharma <vaishalisharmaukl@gmail.com>
2025-11-10 21:38:34 +0530

    name and greeting concatenating

--- Javascript/challenge 6/index.js
+++ Javascript/challenge 6/index.js
@@ -1,8 +1,18 @@
-let username = "nod"
+// let username = "nod"
 
 // to create a string that show message
-let message = "You have three new notifications"
+// let message = "You have three new notifications"
 
 // create a varibale messageTouser containing the message to user.
-let messageToUser = message + ", " + username;
-console.log(messageToUser);
+// let messageToUser = message + ", " + username;
+// console.log(messageToUser);
+
+// challenge 3
+// creating two varible name and greeting and a third string that concatinate the two strings together
+
+let myName = "noddy"
+let greeting = "Hi, my name is "
+
+let myGreetings = greeting + myName
+
+console.log(myGreetings);
