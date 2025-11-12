18503c78cb0d3cf30e913a32f96eb17b2e490efc
vsharma <vaishalisharmaukl@gmail.com>
2025-11-12 21:53:04 +0530

    variables practice complete

---
+++ Javascript/challenge 8/index.js
@@ -0,0 +1,13 @@
+//challenge-1 (Variables practice)
+
+// create two variables: firstName and lastName
+let firstName="noddy"
+let lastName="cartoon"
+
+// Concatenate the two variables into the third variable called fullName
+let fullName = firstName + " " + lastName;
+
+// Log the fullName on console
+console.log(fullName);
+
+
