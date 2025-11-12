980956480cc513328b7c5f7369983962278edc07
vsharma <vaishalisharmaukl@gmail.com>
2025-11-12 22:13:27 +0530

    strings and numbers complete

--- Javascript/challenge 8/index.js
+++ Javascript/challenge 8/index.js
@@ -47,3 +47,16 @@ remove1Point()
 // 3+3+3+3=12-1=11-1=10
 // Call the functions to that the line below logs out 10
 console.log(myPoints);
+
+// =================================================================
+
+// challenge-4 (strings and numbers)
+
+// Try to predict what each of the lines will log out
+console.log("2" + 2) // "22"
+console.log(11 + 7) //18
+console.log(6 + "5") // "65"
+console.log("My points: " + 5 + 9) // "My points: 59"
+// Note: the above is string + number = string so its 59 not 14
+console.log(2 + 2) // 4
+console.log("11" + "14") // "1114"
