2deaba60a598dec546a85b51cd2db555eda2727e
vsharma <vaishalisharmaukl@gmail.com>
2025-11-12 22:07:25 +0530

    increment decrement complete

--- Javascript/challenge 8/index.js
+++ Javascript/challenge 8/index.js
@@ -10,7 +10,7 @@ let fullName = firstName + " " + lastName;
 // Log the fullName on console
 console.log(fullName);
 
-
+// ====================================================================
 //challenge-2(Concatenate two strings)
 
 let name= "Linda"
@@ -24,3 +24,26 @@ function greet(){
 }
 
 greet();
+
+// ===================================================================
+//challenge-3 (Increment and decrement)
+
+let myPoints = 3;
+// create two function,add3Points() and remove1Point() and have them 
+// add/remove points to/from myPoints variable
+function add3Points(){
+    myPoints+=3;
+}
+function remove1Point(){
+    myPoints-=1
+}
+
+add3Points()
+add3Points()
+add3Points()
+
+remove1Point()
+remove1Point()
+// 3+3+3+3=12-1=11-1=10
+// Call the functions to that the line below logs out 10
+console.log(myPoints);
