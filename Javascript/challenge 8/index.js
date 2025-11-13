c1671da0ff1d7e7c67d51b28018be792504de1b0
vsharma <vaishalisharmaukl@gmail.com>
2025-11-13 22:27:40 +0530

    calculator challenge complete

--- Javascript/challenge 8/index.js
+++ Javascript/challenge 8/index.js
@@ -75,3 +75,37 @@ let pEl=document.getElementById("error")
 function errorMessage(){
     pEl.textContent="Something went wrong, please try again";
 }
+
+// Challenge-6 (Calculator challenge)
+
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let sumEl=document.getElementById("sum-el")
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+function add(){
+  let sum = num1 + num2;
+    sumEl.textContent = "Sum: " +sum;
+    
+}
+function subtract(){
+    let diff = num1 -num2;
+    sumEl.textContent ="Sum: " +diff;
+}
+function divide(){
+    let rem = num1 / num2;
+    sumEl.textContent ="Sum: " +rem;
+}
+function multiply(){
+    let prod = num1 * num2;
+    sumEl.textContent ="Sum: " +prod;
+}
