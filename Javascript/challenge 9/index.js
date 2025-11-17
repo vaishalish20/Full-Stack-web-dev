c9fc74ed3a6e46769418791dabacb58d804b698f
vsharma <vaishalisharmaukl@gmail.com>
2025-11-17 22:18:07 +0530

    first if-else statement

--- Javascript/challenge 9/index.js
+++ Javascript/challenge 9/index.js
@@ -17,3 +17,39 @@ if (sum < 21){
     console.log("You are out of game.");
     
 }
+
+// Next Challenge
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 22
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if(age < 21){
+    console.log("You can not enter the club!");
+}else{
+    console.log("Welcome!");
+}
+
+
+
+
+
+// Next Challenge
+// Check if the person is elegible for a birthday card from the King! (100)
+
+// let age = 100;
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+// if(age < 100){
+//     console.log("Not elegible");
+// }else if(age === 100){
+//     console.log("Here is your birthday card from the King!");
+// }else{
+//     console.log("Not elegible, you have already gotten one");
+// }
