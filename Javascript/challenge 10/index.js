7eb717abcc72a64cfe6a7835b53991ac91304e37
vsharma <vaishalisharmaukl@gmail.com>
2025-11-18 21:37:57 +0530

    adding next challenges to separate folder

---
+++ Javascript/challenge 10/index.js
@@ -0,0 +1,35 @@
+// Next Challenge
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 22
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if(age < 21){
+    console.log("You can not enter the club!");
+}else{
+    console.log("Welcome!");
+}
+
+// note: we can use (age < 21) or (age <= 20) both is same thing
+
+
+
+// Next Challenge
+// Check if the person is elegible for a birthday card from the King! (100)
+
+// let age = 100;
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+// if(age < 100){
+//     console.log("Not elegible");
+// }else if(age === 100){
+//     console.log("Here is your birthday card from the King!");
+// }else{
+//     console.log("Not elegible, you have already gotten one");
+// }
