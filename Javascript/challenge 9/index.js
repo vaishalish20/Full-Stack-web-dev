e126c63fbf77aeba120498a49ae03891eda09929
vsharma <vaishalisharmaukl@gmail.com>
2025-11-19 21:48:15 +0530

    completing sum

--- Javascript/challenge 9/index.js
+++ Javascript/challenge 9/index.js
@@ -2,7 +2,7 @@
 // Set their values to a random number between 2-11
 
 let firstCard = 10;
-let secondCard = 2;
+let secondCard = 22;
 // 2. Create a variable, sum, and set it to the sum of the two cards.
 
 let sum = firstCard + secondCard;
@@ -43,21 +43,26 @@ let isAlive = true;
 // 1. Store the message-el paragraph in a variable called messageEl
 let messageEl=document.getElementById("message-el");
 
-// 2. Display the message in the messageEl using messageEl.textContent
+// 2. Store the sum paragraph in a variable called sumEl
+// let sumEl=document.getElementById("sum-el")
+let sumEl=document.querySelector(".sum-el")
+ // 3. Render the sum on the page using this format -> "Sum: 14"
 function startGame(){
-if (sum <= 20) {
-    // console.log("Do you want to draw a new card? 🙂");
-    message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-    // console.log("Wohoo! You've got Blackjack! 🥳");
-    message = "Wohoo! You've got Blackjack! 🥳";
-} else {
-    // console.log("You're out of the game! 😭");
-    message = "You're out of the game! 😭";
-    isAlive = false
-}
-// console.log(isAlive);
-// console.log(message);
+    if (sum <= 20) {
+        // console.log("Do you want to draw a new card? 🙂");
+        message = "Do you want to draw a new card?";
+        sumEl.textContent="Sum: " +  sum;
+    } else if (sum === 21) {
+        // console.log("Wohoo! You've got Blackjack! 🥳");
+        message = "Wohoo! You've got Blackjack!";
+    } else {
+        // console.log("You're out of the game! 😭");
+        message = "You're out of the game!";
+        isAlive = false
+    }
+    // console.log(isAlive);
+    // console.log(message);
+    // 2. Display the message in the messageEl using messageEl.textContent
 messageEl.textContent = message;
 }
 
