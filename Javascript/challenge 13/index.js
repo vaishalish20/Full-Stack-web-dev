89fd8dfcb6fa1b49e7a17cfc261b0e4bd86a3585
vsharma <vaishalisharmaukl@gmail.com>
2025-11-21 23:30:31 +0530

    writing first array based loop in challenge

--- Javascript/challenge 13/index.js
+++ Javascript/challenge 13/index.js
@@ -50,9 +50,20 @@ let messages = [
     "Great"
 ]
 let n = messages.length;
-console.log(n);
+// console.log(n);
 
 // console log the messages array using loop
-for (let i = 0;i < n ; i++){
-    console.log(messages[i]);
+// for (let i = 0;i < n ; i++){
+//     console.log(messages[i]);
+// }
+
+// =====================================================
+let cards = [7,3,9]
+
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+
+for (let i=0;i<cards.length;i++){
+    console.log(cards[i]);
+    
 }
