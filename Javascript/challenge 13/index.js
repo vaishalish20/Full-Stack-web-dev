bdbe96a2ee08db8a1d1f21af8070c366f5a42029
vsharma <vaishalisharmaukl@gmail.com>
2025-11-21 23:11:39 +0530

    loop practice challenges

---
+++ Javascript/challenge 13/index.js
@@ -0,0 +1,27 @@
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//    START           FINISH       STEP SIZE
+// for ( let count = 1;  count < 11;  count += 1 )  {
+    
+//     console.log(count)
+
+// }
+
+// Challenge - count to 20
+// for ( let count = 1;  count < 21;  count += 1 )  {
+    
+//     console.log(count)
+
+// }
+//challenge - count 10 to 20
+// for ( let count = 10;  count < 21;  count += 1 )  {
+    
+//     console.log(count)
+
+// }
