e2a8dbba165e53b267ca25cae3738baf022da7dc
vsharma <vaishalisharmaukl@gmail.com>
2025-11-21 23:27:23 +0530

    combining arrays and loops

--- Javascript/challenge 13/index.js
+++ Javascript/challenge 13/index.js
@@ -26,16 +26,33 @@
 
 // }
 
-for (let i = 0; i < 6; i+=1) {
-    console.log(i);
+// for (let i = 0; i < 6; i+=1) {
+//     console.log(i);
 
-}
-
+// }
+// note: i can be used again, as it was used inside the for loop, so it is kind of local scope variable
 // challenge
 // Create a for loop that counts from 10 to 100 in steps of 10
 // Use console.log to log out the numbers
 
-for (let count = 10; count <= 100; count += 10) {
-    console.log(count);
+// for (let count = 10; count <= 100; count += 10) {
+//     console.log(count);
 
+// }
+//==================================================================================
+// Arrays and loop
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great to hear",
+    "Great"
+]
+let n = messages.length;
+console.log(n);
+
+// console log the messages array using loop
+for (let i = 0;i < n ; i++){
+    console.log(messages[i]);
 }
