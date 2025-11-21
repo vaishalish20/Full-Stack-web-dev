a7b587040e921f81d87b230eef7fc30eedc3c563
vsharma <vaishalisharmaukl@gmail.com>
2025-11-21 22:42:47 +0530

    complex datatype

--- Javascript/challenge 12/index.js
+++ Javascript/challenge 12/index.js
@@ -2,15 +2,17 @@ let featuredPosts = ["Check out my Netflix Clone",
     "Here's the code for my Project",
     "I've just relaunched my portfolio"]
 
-console.log(featuredPosts[0]);
-
+// console.log(featuredPosts[0]);
+// console.log(featuredPosts.length);
+// =============================================================================
+//challenge
 // Create an array that lists your i.e. experience, education, licenses, skills or similar
 // The items of the array should be strings
 
 let skills = ["HTML", "CSS", "JS", "NodeJS", "SQL"]
-
-console.log(skills[1]);
-
+// console.log(skills[1]);
+// ==========================================================================
+// challenge 
 let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
 
 // Make the following appear in the console:
@@ -18,7 +20,20 @@ let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People coun
 // People counter for Norstat
 // CEO at Scrimba
 
-console.log(experience[1]);
-console.log(experience[2]);
-console.log(experience[0]);
+// console.log(experience[1]);
+// console.log(experience[2]);
+// console.log(experience[0]);
 
+// ==========================================================================
+// Array - ordered list of items - coposite / complex data type
+
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+let myName = "Noddy"
+let age =  24
+let likePizza = true
+let description = [myName , age , likePizza]
+
+console.log(description[0] + " is " + typeof(description[0]));
+console.log(description[1] + " is " + typeof(description[1]));
+console.log(description[2] + " is " + typeof(description[2]));
