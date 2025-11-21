e71e96ee55dc9cf31f57b33ed0d36e57d601415a
vsharma <vaishalisharmaukl@gmail.com>
2025-11-21 23:16:37 +0530

    write first loop

--- Javascript/challenge 13/index.js
+++ Javascript/challenge 13/index.js
@@ -8,20 +8,34 @@
 
 //    START           FINISH       STEP SIZE
 // for ( let count = 1;  count < 11;  count += 1 )  {
-    
+
 //     console.log(count)
 
 // }
 
 // Challenge - count to 20
 // for ( let count = 1;  count < 21;  count += 1 )  {
-    
+
 //     console.log(count)
 
 // }
 //challenge - count 10 to 20
 // for ( let count = 10;  count < 21;  count += 1 )  {
-    
+
 //     console.log(count)
 
 // }
+
+for (let i = 0; i < 6; i+=1) {
+    console.log(i);
+
+}
+
+// challenge
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+for (let count = 10; count <= 100; count += 10) {
+    console.log(count);
+
+}
