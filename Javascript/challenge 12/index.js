7fd270cff7849d10af9961dbd84694e4889e6978
vsharma <vaishalisharmaukl@gmail.com>
2025-11-21 22:27:22 +0530

    learning about arrays and index in arrays

---
+++ Javascript/challenge 12/index.js
@@ -0,0 +1,12 @@
+let featuredPosts = ["Check out my Netflix Clone",
+    "Here's the code for my Project",
+    "I've just relaunched my portfolio"]
+
+console.log(featuredPosts[0]);
+
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let skills = ["HTML", "CSS", "JS", "NodeJS", "SQL"]
+
+console.log(skills[1]);
