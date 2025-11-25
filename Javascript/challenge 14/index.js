300bda35d8afadd5b7945add1349fa7cf1ff157c
vsharma <vaishalisharmaukl@gmail.com>
2025-11-25 21:49:42 +0530

    learning about return

---
+++ Javascript/challenge 14/index.js
@@ -0,0 +1,26 @@
+let player1Time = 101
+let player2Time = 10
+
+function getFastestRaceTime(){
+    if(player1Time < player2Time){
+        return player1Time;
+    }else if(player2Time < player1Time){
+        return player2Time;
+    }else{
+        return player1Time;
+    }
+}
+
+// let fastestRace = getFastestRaceTime()
+// console.log(fastestRace)
+
+// Write a function that returns the total race time
+// Call/invoke the function and store the returned value in a new variable
+// Finally, log the variable out
+
+function totalRaceTime(){
+    return player1Time + player2Time;
+}
+
+let totalTime = totalRaceTime();
+console.log(totalTime);
