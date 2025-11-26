b9ee5410ac86ad1785b107e5fd711ad4ff413636
vsharma <vaishalisharmaukl@gmail.com>
2025-11-26 22:29:56 +0530

    OR operator challenge

--- Javascript/challenge 15/index.js
+++ Javascript/challenge 15/index.js
@@ -12,14 +12,34 @@ function generateCertificate() {
 generateCertificate()
 
 
+// ====================================
 
-
-let hasSolvedChallenge = false
+let hasSolvedChallenge = true
 let hasHintsLeft = false
 
 // Create an if statement that checks that both variables are false.
 // If so, run the showSolution() function
 
+if(hasSolvedChallenge !== true && hasHintsLeft !== true){
+    showSolution();
+}
 function showSolution() {
     console.log("Showing the solution....")
+}
+
+// Note: And means both the condition should be true and in case of OR either of the conditions are true.
+
+// =================================================
+
+// Create two boolean variables, likesDocumentaries and likesStartups
+// Use an OR statement (||) to call recommendMovie() if either of those variables are true
+
+let likesDocumentaries = false;
+let likesStartups = true;
+
+if (likesDocumentaries === true || likesStartups === true){
+    recommendMovie();
+}
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
 }
