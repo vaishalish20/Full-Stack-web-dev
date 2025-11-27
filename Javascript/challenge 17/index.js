a6e3c2b7f43d1d211b4622fb862dde18e2ebd1c6
vsharma <vaishalisharmaukl@gmail.com>
2025-11-27 22:35:17 +0530

    completing challenge 2

--- Javascript/challenge 17/index.js
+++ Javascript/challenge 17/index.js
@@ -3,7 +3,7 @@
 let randomNumber = Math.floor(Math.random() *6 ) + 1;
 console.log(randomNumber);
 
-// challenges: final
+// challenges 1: objects and functions
 // Create a person object that contains three keys: name, age, and county.
 // Use yourself as an example to set the values for name, age, and country
 let person = {
@@ -25,3 +25,31 @@ function logData(){
 // Call the logData() function to verify that it works
 logData();
 
+// challenge 2" if-else
+
+let age = 0
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+if(age < 6 ){
+    console.log("free");
+    
+}else if(age >= 6 && age <= 17){
+    console.log("child discount");
+    
+}else if(age >= 18 && age <= 26){
+    console.log("studnet discount");
+    
+}else if(age >=27 && age<= 66){
+    console.log("full price");
+    
+}else if(age >= 66){
+    console.log("senior citizen discount");
+    
+}
