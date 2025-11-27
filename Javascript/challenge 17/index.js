4f6a403f5b108652cf3587ee50c498f0549eb5f7
vsharma <vaishalisharmaukl@gmail.com>
2025-11-27 22:24:58 +0530

    completing challenge 1

--- Javascript/challenge 17/index.js
+++ Javascript/challenge 17/index.js
@@ -2,3 +2,26 @@
 
 let randomNumber = Math.floor(Math.random() *6 ) + 1;
 console.log(randomNumber);
+
+// challenges: final
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
+let person = {
+    name : "Noddy",
+    age : 20,
+    country : "Toyland"
+}
+
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+
+function logData(){
+    let string = person.name + " is " + person.age + " years old and lives in " + person.country
+    console.log(string);
+    
+}
+// "Noddy is 20 years old and lives in Toyland"
+
+// Call the logData() function to verify that it works
+logData();
+
