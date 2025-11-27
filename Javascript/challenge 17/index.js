d706a5720e345f0e07519085d941f79adca32661
vsharma <vaishalisharmaukl@gmail.com>
2025-11-27 22:40:03 +0530

    completing challenge 3

--- Javascript/challenge 17/index.js
+++ Javascript/challenge 17/index.js
@@ -53,3 +53,22 @@ if(age < 6 ){
     console.log("senior citizen discount");
     
 }
+
+//challenge 3:for loop
+
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"];
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indonesia
+- Pakistan
+*/
+
+for(let i=0; i<largeCountries.length; i++){
+    console.log(largeCountries[i]);
+    
+}
