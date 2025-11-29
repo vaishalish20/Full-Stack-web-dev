1294b0a49d7636e4c8c937a66aa2d7c3273f2be0
vsharma <vaishalisharmaukl@gmail.com>
2025-11-29 22:56:50 +0530

    replacing let with const

--- Javascript/challenge-18-Chrome-extension/index.js
+++ Javascript/challenge-18-Chrome-extension/index.js
@@ -1,6 +1,14 @@
-let inputBtn =  document.getElementById("input-btn")
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+let myLeads = [];
+// inputEl -> should be assigned to the text input field
+//note: using const datatype instead of let to not change its value
+const inputEl =  document.getElementById("input-el")
+
+const inputBtn =  document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function (){
     console.log("Button clicked !");
     
 })
+
