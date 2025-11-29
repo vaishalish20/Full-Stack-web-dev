78eb12f85ed8e23c802db363099fc6165cd6661a
vsharma <vaishalisharmaukl@gmail.com>
2025-11-29 23:05:23 +0530

    learning about let and const in challenge

--- Javascript/challenge 19/index.js
+++ Javascript/challenge 19/index.js
@@ -8,3 +8,29 @@ boxEl.addEventListener("click",function(){
     console.log("I want to open the box!");
     
 })
+
+//learning about let and const
+
+const playerNumber = "Noddy";
+let credits = 44;
+
+// If possible, use const. If not, use let.
+
+// Which variables below should be changed from let to const?
+
+// The customer wants to order some stuff. Here are the details:
+const basePrice = 520   //should be const
+const discount = 120 //should be const
+let shippingCost = 12  
+let shippingTime = "5-12 days"
+
+// Whops! Turns out the shipping will be a bit more complex
+shippingCost = 15
+shippingTime = "7-14 days"
+
+// Calculating the full price
+// note: here fullPrice is not assigned further so it can be changed to const because value is fixed.
+const fullPrice = basePrice - discount + shippingCost
+
+// Finally, notifying the customer
+console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
