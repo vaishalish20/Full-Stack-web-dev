3c3e2fb15b0b0fe164290e5c141c5fc2ce5a90c9
vsharma <vaishalisharmaukl@gmail.com>
2025-11-29 23:28:03 +0530

    adding for push function and for loop

--- Javascript/challenge-18-Chrome-extension/index.js
+++ Javascript/challenge-18-Chrome-extension/index.js
@@ -1,14 +1,24 @@
 // Create two variables:
 // myLeads -> should be assigned to an empty array
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 // inputEl -> should be assigned to the text input field
 //note: using const datatype instead of let to not change its value
 const inputEl =  document.getElementById("input-el")
 
 const inputBtn =  document.getElementById("input-btn")
 
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
 inputBtn.addEventListener("click", function (){
-    console.log("Button clicked !");
-    
+    // myLeads.push("www.awesomelead.com")  
+
+    // Push the value from the inputEl into the myLeads array 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    myLeads.push(inputEl.value)
+    console.log(myLeads);
 })
+
+for(let i=0;i < myLeads.length;i++){
+    console.log(myLeads[i]);   
+}
 
