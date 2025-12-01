41de175081633632dff1f316f9e79fa0c9d95974
vsharma <vaishalisharmaukl@gmail.com>
2025-12-01 21:26:40 +0530

    creating multiple lines template strings

--- Javascript/challenge 21/index.js
+++ Javascript/challenge 21/index.js
@@ -2,8 +2,15 @@
 
 const recipient = "James"
 
+// Create a new variable, sender, and set its value to your name
+const sender = "Noddy"
+// Use your sender variable instead of "Per"
+
 // Refactor the email string to use template strings
 // const email = "Hey " + recipient + "! How is it going? Cheers Per"
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+const email = `
+Hey ${recipient}! 
+How is it going? Cheers 
+${sender}`
 
 console.log(email)
