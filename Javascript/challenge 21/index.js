8b48e6c7758f811707619298a6428c50e9895794
vsharma <vaishalisharmaukl@gmail.com>
2025-12-01 21:19:45 +0530

    learning more about template strings

---
+++ Javascript/challenge 21/index.js
@@ -0,0 +1,9 @@
+// template strings/literals
+
+const recipient = "James"
+
+// Refactor the email string to use template strings
+// const email = "Hey " + recipient + "! How is it going? Cheers Per"
+const email = `Hey ${recipient}! How is it going? Cheers Per`
+
+console.log(email)
