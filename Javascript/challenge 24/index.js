e6c464ba436605e63f154e13029ef8b0a80cbaf3
vsharma <vaishalisharmaukl@gmail.com>
2025-12-06 18:49:39 +0530

    adding template literals and emoji as params

--- Javascript/challenge 24/index.js
+++ Javascript/challenge 24/index.js
@@ -2,8 +2,14 @@ const welcomeEl = document.getElementById("welcome-el")
 
 // Give the function a parameter, greeting, that replaces "Welcome back"
 // note; providing this function multiple parameters
-function greetUser(greeting,username){
-    welcomeEl.textContent = greeting + " " + username;
+
+// adding template literals to avoid + when adding more variables
+// add the ability to add an emoji too to this function
+function greetUser(greeting,username, emoji){
+    welcomeEl.textContent = `${greeting} ${username} ${emoji}`;
 }
 
-greetUser("Hello","Noddy");
+greetUser("Hello","Noddy","👋");
+
+// ======================================================================================
+
