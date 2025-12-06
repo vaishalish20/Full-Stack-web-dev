5929a523ae9d479e3bd6d59a444bf6935d6c920f
vsharma <vaishalisharmaukl@gmail.com>
2025-12-06 22:07:17 +0530

    challenge 5 complete

--- Javascript/challenge 25-js problems/index.js
+++ Javascript/challenge 25-js problems/index.js
@@ -72,3 +72,31 @@ logBtn.addEventListener("click",function(){
   let winnerScore = data[0].score
   console.log(winnerScore);
 })
+
+// Challenge 5 : generate sentences
+
+// The generateSentence(desc, arr) takes two parameterer: a description and an array.
+// It should return a string based upon the description and array.
+
+// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
+// it should return the string: "The 3 largest countries are China, India, USA"
+
+// Example 2: If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
+// "The 2 best fruits are Apples, Bananas"
+
+// Use both a for loop and a template string to solve the challenge
+function generateSentence(desc, arr) {
+    let baseString = `The ${arr.length} ${desc} are `
+    let lastIndex = arr.length -1;
+    for(let i=0; i<arr.length;i++){
+        if( i === lastIndex){
+            baseString+=arr[i] 
+        }else{
+            baseString+=arr[i] + ", "
+        }
+    }
+    return baseString
+}
+
+const output = generateSentence("largest countries", ["China", "India", "USA"])
+console.log(output);
