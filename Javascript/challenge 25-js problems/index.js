fa336dbfd3fd4dfd80559559c16ed656bdd3c9c4
vsharma <vaishalisharmaukl@gmail.com>
2025-12-06 23:46:09 +0530

    challenge 8 completed

--- Javascript/challenge 25-js problems/index.js
+++ Javascript/challenge 25-js problems/index.js
@@ -134,6 +134,16 @@
 // Round the price in the button down to two decimal places.
 // Don't know which method to use? Google it!
 
-const totalPrice = 420.69235632455
-const btn = document.getElementById("purchase-btn")
-btn.textContent = `Buy €${totalPrice.toFixed(2)}`
+// const totalPrice = 420.69235632455
+// const btn = document.getElementById("purchase-btn")
+// btn.textContent = `Buy €${totalPrice.toFixed(2)}`
+
+// challege 8 :  convert string to number
+
+// Challenge:
+// The toFixed() method doesn't work anymore. Can you make it work?
+// Google the error message if you're unsure about how to do it
+
+const totalPrice = "420.69235632455"
+const btn = document.getElementById("buy-btn")
+btn.textContent = `Buy €${ Number(totalPrice).toFixed(2) }`
