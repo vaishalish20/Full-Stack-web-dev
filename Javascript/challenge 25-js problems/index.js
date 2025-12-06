e4488a06790ec1db4c6b973300be29c4248140d6
vsharma <vaishalisharmaukl@gmail.com>
2025-12-06 23:24:26 +0530

    challenge 6 complete

--- Javascript/challenge 25-js problems/index.js
+++ Javascript/challenge 25-js problems/index.js
@@ -1,40 +1,40 @@
 // challenge 1: let and const
 // SETTING THE STAGE
-const player = "Noddy"
-const opponent = "Nick"
-const game = "AmazingFighter"
-let points = 0
-let hasWon = false
+// const player = "Noddy"
+// const opponent = "Nick"
+// const game = "AmazingFighter"
+// let points = 0
+// let hasWon = false
 
-// PLAYING THE GAME
-points += 100
-hasWon = true
+// // PLAYING THE GAME
+// points += 100
+// hasWon = true
 
-// ANNOUNCING THE WINNER
-if (hasWon) {
-    console.log(`${player} got ${points} points and won the ${game} game!`)
-} else {
-    console.log(`The winner is ${opponent}! ${player} lost the game`)
-}
+// // ANNOUNCING THE WINNER
+// if (hasWon) {
+//     console.log(`${player} got ${points} points and won the ${game} game!`)
+// } else {
+//     console.log(`The winner is ${opponent}! ${player} lost the game`)
+// }
 
 // Go through all variables and decide if they should be let or const
 // Change the console logs to use template strings instead of double quotes
 
 // challenge 2: log out items in an array
 
-let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+// let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
 
 // Create a function that takes a single parameter, an array,
 // and logs all the items of the array to the console.
 // Call the function while passing in myCourses as an argument
 
-function traverse(arr) {
-    for (let i = 0;i< arr.length ; i++){
-        console.log(arr[i]);
-    }
-}
+// function traverse(arr) {
+//     for (let i = 0;i< arr.length ; i++){
+//         console.log(arr[i]);
+//     }
+// }
 
-traverse(myCourses)
+// traverse(myCourses)
 
 // challenge 3 : save to localStorage 
 
@@ -42,36 +42,36 @@ traverse(myCourses)
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
 
-localStorage.setItem("myName","Noddy")
-localStorage.setItem("myMessage","Hello World")
-let messageValue = localStorage.getItem("myMessage")
-console.log(messageValue);
+// localStorage.setItem("myName","Noddy")
+// localStorage.setItem("myMessage","Hello World")
+// let messageValue = localStorage.getItem("myMessage")
+// console.log(messageValue);
 
 
 // challenge 4 : addEventListener and objects in array
 
-let data = [
-    {
-        player: "Jane",
-        score: 52
-    }, 
-    {
-        player: "Mark",
-        score: 41
-    }
-]
+// let data = [
+//     {
+//         player: "Jane",
+//         score: 52
+//     }, 
+//     {
+//         player: "Mark",
+//         score: 41
+//     }
+// ]
 
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
 
-const logBtn =  document.getElementById("log-btn")
+// const logBtn =  document.getElementById("log-btn")
 
-logBtn.addEventListener("click",function(){
+// logBtn.addEventListener("click",function(){
 
-  let winnerScore = data[0].score
-  console.log(winnerScore);
-})
+//   let winnerScore = data[0].score
+//   console.log(winnerScore);
+// })
 
 // Challenge 5 : generate sentences
 
@@ -85,18 +85,45 @@ logBtn.addEventListener("click",function(){
 // "The 2 best fruits are Apples, Bananas"
 
 // Use both a for loop and a template string to solve the challenge
-function generateSentence(desc, arr) {
-    let baseString = `The ${arr.length} ${desc} are `
-    let lastIndex = arr.length -1;
-    for(let i=0; i<arr.length;i++){
-        if( i === lastIndex){
-            baseString+=arr[i] 
-        }else{
-            baseString+=arr[i] + ", "
-        }
+// function generateSentence(desc, arr) {
+//     let baseString = `The ${arr.length} ${desc} are `
+//     let lastIndex = arr.length -1;
+//     for(let i=0; i<arr.length;i++){
+//         if( i === lastIndex){
+//             baseString+=arr[i] 
+//         }else{
+//             baseString+=arr[i] + ", "
+//         }
+//     }
+//     return baseString
+// }
+
+// const output = generateSentence("largest countries", ["China", "India", "USA"])
+// console.log(output);
+
+
+// challenge 6: render images
+
+// Create a function that renders the three team images
+// Use a for loop, template strings (``), plus equals (+=)
+// .innerHTML to solve the challenge.
+
+const containerEl = document.getElementById("container")
+
+const imgs = [
+    "images/img1.png",
+    "images/img2.jpg",
+    "images/img3.png"
+]
+console.log();
+
+//  <img class="team-img" src="images/img1.png">
+function renderTeamImages() {
+    let imgsDOM = ""
+    for (let i = 0; i < imgs.length; i++) {
+        imgsDOM += `<img alt="places" class="team-img" src="${imgs[i]}">`
     }
-    return baseString
+    containerEl.innerHTML = imgsDOM
 }
 
-const output = generateSentence("largest countries", ["China", "India", "USA"])
-console.log(output);
+renderTeamImages()
