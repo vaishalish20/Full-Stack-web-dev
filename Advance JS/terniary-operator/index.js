3fa70f9cd10eb52ebd060969bcd7ceff7a0899c0
vsharma <vaishalisharmaukl@gmail.com>
2025-12-19 19:30:26 +0530

    adding syntax as note

--- Advance JS/terniary-operator/index.js
+++ Advance JS/terniary-operator/index.js
@@ -12,3 +12,8 @@ const exerciseTimeMins = 50;
 // Doing the same thing with ternary operator
 const message = exerciseTimeMins < 30 ? 'You need to work harder':'Doing Good';
 console.log(message);
+
+// Note: Syntax of the Ternary Operator
+
+// condition if true expression1 and if false expression2
+// condition ? expression1 : expression2 
