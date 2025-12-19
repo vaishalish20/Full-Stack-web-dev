a517274b06ecc4e02e29564f3d139bd6ac96a8d2
vsharma <vaishalisharmaukl@gmail.com>
2025-12-19 19:42:01 +0530

    learning about ternary operator with more complexity

--- Advance JS/terniary-operator/index.js
+++ Advance JS/terniary-operator/index.js
@@ -1,4 +1,4 @@
-const exerciseTimeMins = 50;
+// const exerciseTimeMins = 20;
 
 // Doing it with if-else statement
 // let message = ''
@@ -10,10 +10,27 @@ const exerciseTimeMins = 50;
 // }
 
 // Doing the same thing with ternary operator
-const message = exerciseTimeMins < 30 ? 'You need to work harder':'Doing Good';
-console.log(message);
+// const message = exerciseTimeMins < 30 ? 'You need to work harder':'Doing Good';
+// console.log(message);
 
 // Note: Syntax of the Ternary Operator
 
 // condition if true expression1 and if false expression2
 // condition ? expression1 : expression2 
+
+// ==========================================================
+// Ternary Operator but with complexity
+// let message = ''
+
+// if (exerciseTimeMins < 30){
+//     message = 'You need to work harder'
+// }else if(exerciseTimeMins < 60){
+//     message = 'Doing Good'
+// }else{
+//     message = 'Excellent!'}
+
+// const message = exerciseTimeMins < 30 ? 'You need to work harder': (exerciseTimeMins < 60 ? 'Doing Good':'Excellent!');
+//     console.log(message);
+
+// Note: With multiple condition, it is better to use if-else as that will be easier to read.
+// Above terniary is okay, but not readable if add more conditions.
