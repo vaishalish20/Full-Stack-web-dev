04218f13ba81f7623ea405d2be970b90a46ef1fa
vsharma <vaishalisharmaukl@gmail.com>
2025-12-22 22:20:38 +0530

    learning about clearTimeout

--- Advance JS/setTimeout with params/index.js
+++ Advance JS/setTimeout with params/index.js
@@ -1,11 +1,11 @@
-function displayTrafficLights(light){
-    console.log(light);
+// function displayTrafficLights(light){
+//     console.log(light);
     
-}
+// }
 
 
-setTimeout(displayTrafficLights ,3000 , '🟢')
-displayTrafficLights('🔴')
+// setTimeout(displayTrafficLights ,3000 , '🟢')
+// displayTrafficLights('🔴')
 
 // =============================================
 // challenge
@@ -15,10 +15,16 @@ function logAnswer(name,points){
     
 }
 
-setTimeout(logAnswer, 3000, 'Lima','10')
+const questionTimer = setTimeout(logAnswer, 5000, 'Lima','10')
 console.log('What is the capital of Peru?');
 
 // 1. after a 3 second delay, call the 'logAnswer' function.
 // 2. Make sure logAnswer has all the info it needs
 
 // The answer is 'Lima' and it's 10 points for gettting it right.
+
+document.getElementById('btn').addEventListener('click',function(){
+    clearTimeout(questionTimer);
+    console.log('cancelling.....');
+    
+})
