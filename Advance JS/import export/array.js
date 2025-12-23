79b7702c990b569023bf60f500d57a0f51ad85ba
vsharma <vaishalisharmaukl@gmail.com>
2025-12-23 21:59:32 +0530

    learning about export and import

---
+++ Advance JS/import export/array.js
@@ -0,0 +1,13 @@
+export const arrayObject = [
+    {
+        name: 'noddy',
+        age: 24
+    }, {
+        name: 'bobby',
+        age: 25
+    }
+]
+
+export function add(n1, n2) {
+    return n1 + n2;
+}
---
+++ Advance JS/import export/index.js
@@ -0,0 +1,6 @@
+import {arrayObject,add} from './array.js'
+
+console.log(arrayObject);
+
+
+console.log(add(3,4));
