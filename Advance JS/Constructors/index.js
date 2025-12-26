980a53d15ce8b03272ba08bad5bcb5e092ea8714
vsharma <vaishalisharmaukl@gmail.com>
2025-12-26 23:07:13 +0530

    Error constructor

--- Advance JS/Constructors/index.js
+++ Advance JS/Constructors/index.js
@@ -1,9 +1,29 @@
+// Date constructors
 const dateSnapshot = new Date()
 
-console.log(dateSnapshot);
-console.log(dateSnapshot.toString());
-console.log(typeof dateSnapshot);
+// console.log(dateSnapshot);
+// console.log(dateSnapshot.toString());
+// console.log(typeof dateSnapshot);
 
 
 // Challenge :Find how to console log year from Date consturctor
 console.log(`Copyright ${dateSnapshot.getFullYear().toString()} all rights reserved.`);
+
+
+// Error constructors
+
+function yourName(userName){
+    if(userName){
+        console.log(userName);
+    }else{
+       // console.log('No user name entered');
+       console.log('executed');
+       throw new Error('No username entered')
+       console.log(' not executed');
+        
+    }
+    
+}
+
+yourName()
+//Note: throw new Error will give error and no other statment after throw statement.
