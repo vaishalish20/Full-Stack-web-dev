91ad2821af49183bfe6ab4995a197d6bafcc41de
vsharma <vaishalisharmaukl@gmail.com>
2025-12-26 22:56:39 +0530

    learning about Constructors

---
+++ Advance JS/Constructors/index.js
@@ -0,0 +1,9 @@
+const dateSnapshot = new Date()
+
+console.log(dateSnapshot);
+console.log(dateSnapshot.toString());
+console.log(typeof dateSnapshot);
+
+
+// Challenge :Find how to console log year from Date consturctor
+console.log(`Copyright ${dateSnapshot.getFullYear().toString()} all rights reserved.`);
