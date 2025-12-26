d3c1a46413af3389767b6473b49c37d2158fb544
vsharma <vaishalisharmaukl@gmail.com>
2025-12-26 23:20:20 +0530

    datatype constructors

--- Advance JS/Constructors/index.js
+++ Advance JS/Constructors/index.js
@@ -12,18 +12,33 @@ console.log(`Copyright ${dateSnapshot.getFullYear().toString()} all rights reser
 
 // Error constructors
 
-function yourName(userName){
-    if(userName){
-        console.log(userName);
-    }else{
-       // console.log('No user name entered');
-       console.log('executed');
-       throw new Error('No username entered')
-       console.log(' not executed');
+// function yourName(userName){
+//     if(userName){
+//         console.log(userName);
+//     }else{
+//        // console.log('No user name entered');
+//        console.log('executed');
+//        throw new Error('No username entered')
+//        console.log(' not executed');
         
-    }
+//     }
     
-}
+// }
 
-yourName()
+// yourName()
 //Note: throw new Error will give error and no other statment after throw statement.
+
+//Datetype constructors
+
+String()
+Number()
+Array()
+Object()
+Boolean()
+
+// const person = new Object()
+// Above syntax is rear,we use the following object literal
+const person = {}
+person.name = 'Tom'
+console.log(person);
+
