7679f9bdb5c4abd8a4531afccc0c2d86498be743
vsharma <vaishalisharmaukl@gmail.com>
2025-12-31 21:33:20 +0530

    learning about arrow functions

---
+++ Advance JS/arrow-function/index.js
@@ -0,0 +1,9 @@
+// following is the function expression
+// const getSpendAlert = function(amount){
+//     return `Warning!, you just spent $${amount}`
+// }
+
+const getSpendAlert = (amount) => {
+    return `Warning!, you just spent $${amount}`
+}
+console.log(getSpendAlert(300));
