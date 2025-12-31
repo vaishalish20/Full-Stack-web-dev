0dd19044a14ac2827dd42609b263f994e2f04686
vsharma <vaishalisharmaukl@gmail.com>
2025-12-31 21:28:02 +0530

    function expression challenge completed(question by chatgpt)

--- Advance JS/functions-expression/index.js
+++ Advance JS/functions-expression/index.js
@@ -4,3 +4,25 @@ const funExp = function(){
 }
 
 console.log(funExp());
+
+/*
+Task:
+Create a function expression that:
+Takes two numbers as parameters
+Returns the larger number
+
+Rules:
+Use const
+Do not use arrow functions
+Call the function and log the result
+*/
+
+const returnLarge = function(a,b){
+    if (a>b){
+        return a;
+    }else{
+        return b;
+    }
+}
+
+console.log(`large number is ${returnLarge(2,4)}`);
