2a3a234acecc7db68f566c55c3ed0633982582fb
vsharma <vaishalisharmaukl@gmail.com>
2026-01-01 22:39:37 +0530

    arrow functions important notes

--- Advance JS/arrow-function/index.js
+++ Advance JS/arrow-function/index.js
@@ -3,7 +3,23 @@
 //     return `Warning!, you just spent $${amount}`
 // }
 
-const getSpendAlert = (amount) => {
-    return `Warning!, you just spent $${amount}`
+//with one parameters
+// const getSpendAlert = (amount) => {
+//     return `Warning!, you just spent $${amount}`
+// }
+
+//with no parameters
+// const getSpendAlert = () => {
+//     return `Warning!, you just spent some money`
+// }
+// console.log(getSpendAlert());
+
+//two parameters
+const getSpendAlert = (name, amount) => {
+    return `Warning!, ${name} just spent $${amount}`
 }
-console.log(getSpendAlert(300));
+console.log(getSpendAlert('noddy',300));
+
+//Note: 
+// when we have one parameters we do not need brackets
+// when we have  0 or 2 parameters, we need brackets
