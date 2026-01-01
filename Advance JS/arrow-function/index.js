7ad7f91ad388b46c725a1d473261fc3f633daf44
vsharma <vaishalisharmaukl@gmail.com>
2026-01-01 22:48:33 +0530

    arrow function challenge completed

--- Advance JS/arrow-function/index.js
+++ Advance JS/arrow-function/index.js
@@ -15,11 +15,29 @@
 // console.log(getSpendAlert());
 
 //two parameters
-const getSpendAlert = (name, amount) => {
-    return `Warning!, ${name} just spent $${amount}`
-}
-console.log(getSpendAlert('noddy',300));
+// const getSpendAlert = (name, amount) => {
+//     return `Warning!, ${name} just spent $${amount}`
+// }
+// console.log(getSpendAlert('noddy',300));
+
+//also a arrow function example
+// const getSpendAlert = (name, amount) => `Warning!, ${name} just spent $${amount}`
+
+// console.log(getSpendAlert('noddy',300));
 
 //Note: 
 // when we have one parameters we do not need brackets
 // when we have  0 or 2 parameters, we need brackets
+// can use return keyword when we want to return one line of code
+
+// challenge
+
+// function speedWarning(speed){
+//     return `You are going at ${speed} mph!`
+// }
+
+const speedWarning = speed => `You are going at ${speed} mph!`
+console.log(speedWarning(200));
+
+//write least code possible for arrow functions
+
