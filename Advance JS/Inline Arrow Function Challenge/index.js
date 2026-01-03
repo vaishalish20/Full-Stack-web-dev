1a43ad032cb216054e4dee33f7906ad20bc0651d
vsharma <vaishalisharmaukl@gmail.com>
2026-01-03 22:42:56 +0530

    refactor challenge setup

---
+++ Advance JS/Inline Arrow Function Challenge/index.js
@@ -0,0 +1,10 @@
+import { itemsBoughtArr } from "./itemsBrought";
+
+//challenge : use reduce method to calculate the total cost of items which have been bought.
+ const CalculateTotalCost  = itemsBoughtArr => {
+    
+    const total = itemsBoughtArr.price;
+     return total;
+}
+
+console.log(calculateTotalCost(itemsBoughtArr));
