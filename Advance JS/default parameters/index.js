59bcf0bda02822264951b9b7c412c375b8b6b2bd
vsharma <vaishalisharmaukl@gmail.com>
2026-01-13 22:07:52 +0530

    learning about default paramters

---
+++ Advance JS/default parameters/index.js
@@ -0,0 +1,8 @@
+// we are passing paramters in function defination itself, this is called default paramters.
+function add (num1, num2 = 4){
+    return num1+num2;
+}
+
+let a = add(4)
+console.log(a);
+
