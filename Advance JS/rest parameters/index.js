f19ad4ed2eda8fd35ef9349a5a4ce8bf0446ac0a
vsharma <vaishalisharmaukl@gmail.com>
2026-01-14 22:58:42 +0530

    learning about without rest parameters

---
+++ Advance JS/rest parameters/index.js
@@ -0,0 +1,7 @@
+function setPermissionLevel(permissionLevel, name1, name2, name3){
+    console.log(`${name1} now has ${permissionLevel} level access`);
+    console.log(`${name2} now has ${permissionLevel} level access`);
+    console.log(`${name3} now has ${permissionLevel} level access`);
+    
+}
+setPermissionLevel('admin','Dave','Sally','Mike')
