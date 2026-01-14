44464f7883745f0d52f08a83c91fb157f85bf741
vsharma <vaishalisharmaukl@gmail.com>
2026-01-14 23:52:44 +0530

    recalling about callback function

---
+++ Advance JS/callback function/index.js
@@ -0,0 +1,5 @@
+/* Quiz: list some times that we have passed functions as arguments to other functions.
+1. Array methods: map, forEach, reduce
+2. setTimeout and setInterval
+3. Event Listeners
+*/
