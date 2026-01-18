5e2c7f14655cb5cae7531b6ed0a1914033f980f4
vsharma <vaishalisharmaukl@gmail.com>
2026-01-18 21:38:18 +0530

    adding instructions

--- Advance JS/Super Challenge2/index.js
+++ Advance JS/Super Challenge2/index.js
@@ -1,1 +1,25 @@
-console.log("super challenge");
+// console.log("super challenge");
+function getPropertyHtml() {
+  /*
+  SUPER CHALLENGE 💪
+
+  Render out a card for each of the properties in the propertyForSaleArr array (in the 'properties'
+  folder). Each card should have an image, a property location, a price, a comment and the TOTAL
+  property size in square metres (each object has an array with the size in square metres of the
+  individual rooms).
+
+  If no array of properties is passed to getPropertyHtml, the placeholder property stored in
+  placeholderPropertyObj (in the 'properties' folder) should be rendered instead.
+
+  This is the JS I want you to use to complete this challenge 👇
+  - import/export
+  - .map()
+  - .join()
+  - Object destructuring
+  - .reduce()
+  - Default parameters
+
+  The HTML and CSS have been done for you.
+  This is the HTML template 👇 Replace everything in UPPERCASE with property data.
+  */
+}
