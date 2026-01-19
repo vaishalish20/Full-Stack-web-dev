09f00b00e6eb6669b1f39ec330634cb068b659f7
vsharma <vaishalisharmaukl@gmail.com>
2026-01-19 22:31:56 +0530

    adding object and array

--- Advance JS/Super Challenge2/index.js
+++ Advance JS/Super Challenge2/index.js
@@ -21,5 +21,20 @@ function getPropertyHtml() {
 
   The HTML and CSS have been done for you.
   This is the HTML template 👇 Replace everything in UPPERCASE with property data.
-  */
+
+  <section class="card">
+  <img src="/images/IMAGE">
+  <div class="card-right">
+    <h2>PROPERTY LOCATION</h2>
+    <h3>PRICE GBP</h3>
+    <p>COMMENT</p>
+    <h3>TOTAL SIZE IN SQUARE METRES m&sup2;</h3>
+  </div>
+</section>
+ */
+/***** Modify 👇 by adding an argument to the function call ONLY. *****/
+
+// document.getElementById('container').innerHTML = getPropertyHtml()
+
+ 
 }
